db09241ff2f08e96ceef654d38af225ee34a2814
Andrew <zonrzero@gmail.com>
2020-11-29 23:19:27 -0800

    Adds Readme, license, and cleans up screencapture.py some.

--- screencapture.py
+++ screencapture.py
@@ -1,14 +1,12 @@
 import cv2
 import numpy as np
-from ctypes import *
 import win32ui
 import win32con
 import win32api
 import win32gui
-import asyncio
-# from asyncio.queues import *
 import threading
 import queue
+
 
 def get_windows_list():
     def _window_callback(hwnd, all_windows):
@@ -16,6 +14,7 @@ def get_windows_list():
     windows = []
     win32gui.EnumWindows(_window_callback, windows)
     return windows
+
 
 def get_windows_bytitle(title_text, exact = False):
     def _window_callback(hwnd, all_windows):
@@ -76,7 +75,7 @@ def grab_window(title="Foo"):
     # 0,0 instead of top and left.
     writer_context.BitBlt((0, 0), (width, height), source_context, (0, 0), win32con.SRCCOPY)
 
-    image_array = np.frombuffer(bmp_object.GetBitmapBits(True), dtype='uint8')  # These are signed
+    image_array = np.frombuffer(bmp_object.GetBitmapBits(True), dtype='uint8')  # The results are signed.
     image_array.shape = (height, width, 4)
 
     source_context.DeleteDC()
@@ -116,35 +115,3 @@ def test_screencapture():
 
 if __name__ == '__main__':
     test_screencapture()
-
-# def grab_screen(region=None):
-#     hwin = win32gui.GetDesktopWindow()
-#
-#     if region:
-#         left, top, x2, y2 = region
-#         width = x2 - left + 1
-#         height = y2 - top + 1
-#     else:
-#         width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
-#         height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
-#         left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
-#         top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
-#
-#     hwindc = win32gui.GetWindowDC(hwin)
-#     srcdc = win32ui.CreateDCFromHandle(hwindc)
-#     memdc = srcdc.CreateCompatibleDC()
-#     bmp = win32ui.CreateBitmap()
-#     bmp.CreateCompatibleBitmap(srcdc, width, height)
-#     memdc.SelectObject(bmp)
-#     memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
-#
-#     signedIntsArray = bmp.GetBitmapBits(True)
-#     img = np.fromstring(signedIntsArray, dtype='uint8')
-#     img.shape = (height, width, 4)
-#
-#     srcdc.DeleteDC()
-#     memdc.DeleteDC()
-#     win32gui.ReleaseDC(hwin, hwindc)
-#     win32gui.DeleteObject(bmp.GetHandle())
-#
-#     return cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
